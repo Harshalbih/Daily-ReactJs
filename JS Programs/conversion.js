857be527fe1175973322e03c827eb3a56831a207
Harshal Bihade <63906732+Harshalbih@users.noreply.github.com>
2023-07-22 18:31:17 +0530

    added conversion

---
+++ JS Programs/conversion.js
@@ -0,0 +1,9 @@
+//String to array
+const data= "dlehi, mumbai, pune";
+const toarr= data.split(',')
+console.log(toarr);
+
+//array to string
+const adata = ['delhi', 'mumbai', 'pune'];
+const tostring = adata.join(',')
+console.log(tostring);
