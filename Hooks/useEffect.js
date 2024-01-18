0fd66556cd050c91364593dc0c1b8b532f3dccb9
Harshal Bihade <63906732+Harshalbih@users.noreply.github.com>
2024-01-18 23:46:05 +0530

    Update useEffect.js
    Count effect updated

--- Hooks/useEffect.js
+++ Hooks/useEffect.js
@@ -58,3 +58,23 @@ useEffect(() => {
       subscription.unsubscribe();
     };
   });
+
+//HookCounterOne.js File
+import { useState, useEffect } from "react";
+ 
+function HookCounterOne() {
+    const [count, setCount] = useState(0);
+ 
+    useEffect(() => {
+        document.title = `You clicked ${count} times`;
+    }, [count]);
+ 
+    return (
+        <div>
+            <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+                Click {count} times{" "}
+            </button>
+        </div>
+    );
+}
+export default HookCounterOne;
