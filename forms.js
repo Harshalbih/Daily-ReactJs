756eba71e807e4f49309322b0ed0facd5677d08a
Harshal Bihade <63906732+Harshalbih@users.noreply.github.com>
2024-01-22 23:06:41 +0530

    Update forms.js

--- forms.js
+++ forms.js
@@ -57,3 +57,26 @@ function myForm (){
         </form>
     )
 }
+
+//Form revise
+import { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+
+function MyForm() {
+  const [name, setName] = useState("");
+
+  return (
+    <form>
+      <label>Enter your name:
+        <input
+          type="text" 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+    </form>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<MyForm />);
