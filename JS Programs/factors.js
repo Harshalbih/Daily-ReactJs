e6e0f16b2a1857a27b750317ea5a298c685da5f3
Harshal <bihadeharsh@gmail.com>
2024-02-23 21:52:10 +0530

    js que

--- JS Programs/factors.js
+++ JS Programs/factors.js
@@ -4,3 +4,18 @@ for (let i=0; i<=num; i++){
         console.log(i);
     }
 }
+
+//unique way
+function factorial(number) { 
+
+    if (number === 0 || number === 1) { 
+  
+      return 1; 
+  
+    } else { 
+  
+      return number * factorial(number – 1); 
+  
+    } 
+  
+  } 
