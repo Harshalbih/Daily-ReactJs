9235fb1081e0525413886e808848b6269c238c80
Harshal <bihadeharsh@gmail.com>
2024-03-19 22:42:32 +0530

    added useReducer

---
+++ Hooks/useReducer.js
@@ -0,0 +1,37 @@
+import React, { useReducer } from "react";
+
+// Defining the initial state and the reducer
+const initialState = 0;
+const reducer = (state, action) => {
+switch (action) {
+	case "add":
+	return state + 1;
+	case "subtract":
+	return state - 1;
+	case "reset":
+	return 0;
+	default:
+	throw new Error("Unexpected action");
+}
+};
+
+const App = () => {
+	// Initialising useReducer hook
+const [count, dispatch] = useReducer(reducer, initialState);
+return (
+	<div>
+	<h2>{count}</h2>
+	<button onClick={() => dispatch("add")}>
+		add
+	</button>
+	<button onClick={() => dispatch("subtract")}>
+		subtract
+	</button>
+	<button onClick={() => dispatch("reset")}>
+		reset
+	</button>
+	</div>
+);
+};
+
+export default App;
