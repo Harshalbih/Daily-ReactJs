645891a793a550233316bc79b2627015e1c9febe
Harshal <bihadeharsh@gmail.com>
2024-04-04 23:21:15 +0530

    updated notes

--- events.js
+++ events.js
@@ -1,34 +1,29 @@
 //Put the shoot function inside the Football component:
-function football(){
-    const shoot = () =>{
-        alert("nice shot")
-    }
+function football() {
+  const shoot = () => {
+    alert("nice shot");
+  };
 
-return (
-    <button onClick={shoot}>Take shot</button>
-)
+  return <button onClick={shoot}>Take shot</button>;
 }
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<football />)
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<football />);
 
 //Send "Goal!" as a parameter to the shoot function, using arrow function:
-function football(){
-    const shoot = (a) =>{
-        alert(a)
-    }
+function football() {
+  const shoot = (a) => {
+    alert(a);
+  };
 
-return (
-    <button onClick={() => shoot("Goal")}>Take shot</button>
-)
+  return <button onClick={() => shoot("Goal")}>Take shot</button>;
 }
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<football />)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<football />);
 
-            //events updated
-            function focused() {
-    var e=document.getElementById('inp');
-    if(confirm('Got em?')) {
-        e.blur();
-    }
+//events updated
+function focused() {
+  var e = document.getElementById("inp");
+  if (confirm("Got em?")) {
+    e.blur();
+  }
 }
