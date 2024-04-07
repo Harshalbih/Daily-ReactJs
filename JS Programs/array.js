37ecb7a73d7841414a57f9ca898311e2605d2616
Harshal <bihadeharsh@gmail.com>
2024-04-07 19:58:56 +0530

    updated array

--- JS Programs/array.js
+++ JS Programs/array.js
@@ -106,3 +106,16 @@ uniqueCount = ["a","b","c","d","d","e","a","b","c","f","g","h","h","h","e","a"];
 var count = {};
 uniqueCount.forEach(function(i) { count[i] = (count[i]||0) + 1;});
 console.log(count);
+
+//sort an array
+var Arr = [1, 7, 2, 8, 3, 4, 5, 0, 9];
+
+for (var i = 1; i < Arr.length; i++)
+    for (var j = 0; j < i; j++)
+        if (Arr[i] < Arr[j]) {
+            var x = Arr[i];
+            Arr[i] = Arr[j];
+            Arr[j] = x;
+        }
+
+console.log(Arr);
