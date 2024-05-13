f00c5bbdb03efe977298d859d3f13f9244d0ff68
Harshal <bihadeharsh@gmail.com>
2024-05-13 21:01:52 +0530

    added new code

--- JS Programs/JSIntQue.js
+++ JS Programs/JSIntQue.js
@@ -49,3 +49,42 @@ function deepClone(obj) {
   }
   sum(10,10);
   //output 20 /// it would be 300 if we don't add usestrict because params will overrider arguments in js
+
+  //For of vs For in
+const array = [1,2,3,4,5]
+array.six = 6
+array.seven = 7
+//This array will now look like [1,2,3,4,5,six:6,seven:7]
+// OF: iterates over values
+for (const value of array ) {   
+  console.log(value)
+}
+// Logs:
+// 1
+// 2
+// 3
+// 4
+// 5
+const string = 'string'
+for (const letter of string) {
+  console.log(letter)
+}
+// 's'
+// 't'
+// 'r'
+// 'i'
+// 'n'
+// 'g'
+
+// IN: iterates over keys
+for (const key in array ) {
+  console.log(key, array[key])
+}
+// Logs:
+// '0' 1
+// '1' 2
+// '2' 3
+// '3' 4
+// '4' 5
+// 'six' 6
+// 'seven' 7
